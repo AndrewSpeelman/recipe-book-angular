69993305b7124cc7652be378474bbb5ba78147d2
Andrew Speelman <as7xf@mst.edu>
2022-06-15 23:20:17 -0500

    cleaning up

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,25 +7,7 @@ import {Subject} from "rxjs";
 @Injectable()
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>()
-
-  // private recipes: Recipe[] = [
-  //   new Recipe("Spaghetti",
-  //     "With meaballs",
-  //     "https://www.simplyrecipes.com/thmb/OCi18J2V8OeKDFV3FxoeKvgq74E=/1423x1067/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-horiz-a-1600-7c8292daa98e4020b447f0dc97a45cb7.jpg",
-  //     [
-  //       new Ingredient('Meat', 1),
-  //       new Ingredient('Noodles', 20)
-  //     ]),
-  //   new Recipe("Second Spaghetti",
-  //     "With meaballs",
-  //     "https://www.simplyrecipes.com/thmb/OCi18J2V8OeKDFV3FxoeKvgq74E=/1423x1067/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-horiz-a-1600-7c8292daa98e4020b447f0dc97a45cb7.jpg",
-  //     [
-  //       new Ingredient('turkey', 2),
-  //       new Ingredient('wheat noods', 6)
-  //     ])
-  // ];
   private recipes: Recipe[] = [];
-
 
   constructor(private shoppingListService: ShoppingListService) {
   }
@@ -41,7 +23,6 @@ export class RecipeService {
 
   getRecipe(index: number) {
     return this.recipes[index];
-
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from "@angular/core";
-import {HttpClient, HttpParams} from "@angular/common/http";
+import {HttpClient} from "@angular/common/http";
 import {RecipeService} from "../recipes/recipe.service";
 import {Recipe} from "../recipes/recipe.model";
-import {exhaustMap, map, take, tap} from "rxjs/operators";
+import {map, tap} from "rxjs/operators";
 import {AuthService} from "../auth/auth.service";
 
 @Injectable({providedIn: 'root'})
@@ -21,7 +21,6 @@ export class DataStorageService {
   }
 
   fetchRecipes() {
-
     return this.http
       .get<Recipe[]>(
         'https://recipebook-shoppinglist-5da6f-default-rtdb.firebaseio.com/recipes.json'
