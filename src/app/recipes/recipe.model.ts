767fb2140a9c2916721bdf55c1af4b05a26985fe
Andrew Speelman <as7xf@mst.edu>
2022-12-20 20:42:54 -0600

    Fixes #11. Corrected layout to accommodate for adding instructions to both viewing a Recipe and when editing a Recipe.

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -5,12 +5,16 @@ export class Recipe {
   public description: string;
   public imagePath: string;
   public websiteURL: string;
-  public ingredients: Ingredient[];
 
   public prepTime: string;
   public cookTime: string;
   public totalTime: string;
   public servings: string;
+
+  public ingredients: Ingredient[];
+  public instructions: Instruction[];
+  //TODO: #14.
+  // public instructions: string[];
 
   constructor(name: string,
               description: string,
@@ -20,7 +24,9 @@ export class Recipe {
               cookTime: string,
               totalTime: string,
               recipeYield: string,
-              ingredients: Ingredient[]) {
+              ingredients: Ingredient[],
+              instructions: Instruction[]) {
+    // instructions: string[]) {
     this.name = name;
     this.description = description;
     this.imagePath = imagePath;
@@ -30,5 +36,17 @@ export class Recipe {
     this.totalTime = totalTime;
     this.servings = recipeYield;
     this.ingredients = ingredients;
+    this.instructions = instructions;
+  }
+}
+
+// TODO: #14 This is the only way we're able to use string interpolation on the instruction... primitive string[] was
+// not working... giving [object Object] when attempting to display.
+export class Instruction {
+
+  public instruction: string;
+
+  constructor(instruction: string) {
+    this.instruction = instruction;
   }
 }
